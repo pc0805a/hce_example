a379ca3e15cb74692bb7d3b9416bc1604a626140
張家瑋 <pc0805a@yahoo.com.tw>
2015-05-13 01:01:12 +0800

    delete useless code

--- app/build/generated/source/r/debug/de/grundid/hcedemo/R.java
+++ app/build/generated/source/r/debug/de/grundid/hcedemo/R.java
@@ -18,8 +18,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int confirm_button=0x7f080002;
         public static final int intro=0x7f080000;
-        public static final int listView=0x7f080001;
+        public static final int message=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -29,7 +30,8 @@ public final class R {
         public static final int activity_into=0x7f060001;
         public static final int aiddescription=0x7f060002;
         public static final int app_name=0x7f060003;
-        public static final int servicedesc=0x7f060004;
+        public static final int input_title=0x7f060004;
+        public static final int servicedesc=0x7f060005;
     }
     public static final class style {
         /** 
--- app/src/main/java/de/grundid/hcedemo/MainActivity.java
+++ app/src/main/java/de/grundid/hcedemo/MainActivity.java
@@ -1,64 +1,58 @@
 package de.grundid.hcedemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.ReaderCallback;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.Button;
 import android.widget.ListView;
-import de.grundid.hcedemo.IsoDepTransceiver.OnMessageReceived;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnMessageReceived, ReaderCallback {
+
+public class MainActivity extends Activity {
 
 	private NfcAdapter nfcAdapter;
 	private ListView listView;
 	private IsoDepAdapter isoDepAdapter;
 
+	private Button confirm_btn;
+	private TextView message_txt;
+
+	private void initViews() {
+		confirm_btn = (Button) findViewById(R.id.confirm_button);
+		message_txt = (TextView) findViewById(R.id.message);
+	}
+
+	private void setListeners() {
+		confirm_btn.setOnClickListener(confirm);
+	}
+
+	private OnClickListener confirm = new OnClickListener(){
+		@Override
+		public void onClick(View v) {
+
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		listView = (ListView)findViewById(R.id.listView);
-		isoDepAdapter = new IsoDepAdapter(getLayoutInflater());
-		listView.setAdapter(isoDepAdapter);
-		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
-				null);
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
-		nfcAdapter.disableReaderMode(this);
-	}
-
-	@Override
-	public void onTagDiscovered(Tag tag) {
-		IsoDep isoDep = IsoDep.get(tag);
-		IsoDepTransceiver transceiver = new IsoDepTransceiver(isoDep, this);
-		Thread thread = new Thread(transceiver);
-		thread.start();
-	}
-
-	@Override
-	public void onMessage(final byte[] message) {
-		runOnUiThread(new Runnable() {
-
-			@Override
-			public void run() {
-				isoDepAdapter.addMessage(new String(message));
-			}
-		});
-	}
-
-	@Override
-	public void onError(Exception exception) {
-		onMessage(exception.getMessage().getBytes());
 	}
 }
